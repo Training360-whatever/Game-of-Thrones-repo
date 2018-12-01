c7f8eb4e49a2441b68a8e2305c2e4853e4f6572c
lammoni <lammoni85@gmail.com>
2018-12-01 19:03:02 +0100

    szervezetek lista

--- js/statistics.js
+++ js/statistics.js
@@ -45,7 +45,7 @@ function writeHouses() {
   var houses = housesSum();
   var szoveg = '<tr><th>name of house</th><th>crest</th><th>number of members</th></tr>';
   for (i = 0; i < houses.length; i++) {
-    szoveg += '<tr><td>' +  houses[i].house + ' </td><td><img src=' +  houses[i].image + ' alt=' +  houses[i].house + '> </td><td>' +  parseInt(houses[i].sum,10) + ' </td></tr>';
+    szoveg += '<tr><td>' +  houses[i].house + ' </td><td><img src=' +  houses[i].image + ' alt=' +  houses[i].house + '> </td><td>' +  parseInt(houses[i].sum, 10) + ' </td></tr>';
   }
   document.querySelector('#solution').innerHTML = '';
   document.querySelector('#solution').innerHTML = szoveg;
@@ -82,7 +82,7 @@ function writeMembers() {
   var de = deadList();
   var listAl = '<div "style=width:50%; float:left;">';
   var listDe = '<div "style=width:50%; float:right;">';
-  var szoveg='<tr><th>alive</th><th>dead</th></tr>';
+  var szoveg = '<tr><th>alive</th><th>dead</th></tr>';
 
   for (var i = 0; i < al.length; i++) {
     listAl += al[i] + '<br>';
@@ -94,16 +94,70 @@ function writeMembers() {
     listDe += de[i] + '<br>';
   }
   listDe += '</div>';
-  szoveg+='<tr><td>'+listAl+'</td><td>'+listDe+'</td></tr>';
-  szoveg+='<tr><th>number of alive members</th><th>number of dead members</th></tr>';
-  szoveg+='<tr><td>'+parseInt(al.length,10)+'</td><td>'+parseInt(de.length,10)+'</td></tr>';
+  szoveg += '<tr><td>' + listAl + '</td><td>' + listDe + '</td></tr>';
+  szoveg += '<tr><th>number of alive members</th><th>number of dead members</th></tr>';
+  szoveg += '<tr><td>' + parseInt(al.length, 10) + '</td><td>' + parseInt(de.length, 10) + '</td></tr>';
 
   // var szoveg = '<tr><th>name of house</th><th>crest</th><th>number of members</th></tr>';
 
   document.querySelector('#solutionMembers').innerHTML = '';
   document.querySelector('#solutionMembers').innerHTML = szoveg;
-  //alert(szoveg);
+  // alert(szoveg);
 }
+
+function orgesList() {
+  var t = [];
+  for (var i = 0; i < gameOfThronesCharacters.length; i++) {
+    if (gameOfThronesCharacters[i].organization) {
+      if (!t.includes(gameOfThronesCharacters[i].organization)) {
+        t.push(gameOfThronesCharacters[i].organization);
+      }
+    }
+  }
+  return t;
+}
+
+
+function orgesInit() {
+  var obj = {};
+  var orges = orgesList();
+  var arr = [];
+  for (var i = 0; i < orges.length; i++) {
+    obj = {};
+    obj.org = orges[i];
+    obj.sum = 0;
+    arr.push(obj);
+  }
+  return arr;
+}
+
+function orgSum() {
+  var szoveg = '';
+  var orges = orgesInit();
+  for (i = 0; i < gameOfThronesCharacters.length; i++) {
+    if ( gameOfThronesCharacters[i].organization) {
+      for (var j = 0; j < orges.length; j++)  {
+        if (gameOfThronesCharacters[i].organization === orges[j].org) {
+          orges[j].sum += 1;
+          j = orges.length;
+        }
+      }
+    }
+  }
+  return orges;
+}
+
+function writeOrgs() {
+  var orges = orgSum();
+  var szoveg = '<tr><th>name of organization</th><th>number of members</th></tr>';
+  for (var i = 0; i < orges.length; i++) {
+    szoveg += '<tr><td>' +  orges[i].org + ' </td><td>' +  parseInt(orges[i].sum, 10) + ' </td></tr>';
+  }
+  document.querySelector('#solution3').innerHTML = '';
+  document.querySelector('#solution3').innerHTML = szoveg;
+}
+
+
 // végrehajtás
 // housesList();
 // housesInit();
@@ -114,12 +168,14 @@ function writeMembers() {
 // btnElement.addEventListener('click', writeHouses() );
 writeHouses();
 writeMembers();
-// write
+writeOrgs();
 
-//arr = aliveList();
-//for (i = 0; i < arr.length; i++) {
- // document.querySelector('#solution').innerHTML += arr[i] + ' <br>';
-//}
+// write switch-el
+
+// arr = aliveList();
+// for (i = 0; i < arr.length; i++) {
+// document.querySelector('#solution').innerHTML += arr[i] + ' <br>';
+// }
 // for (i = 0; i < arr.length; i++) {
 // document.querySelector('#solution').innerHTML += '<div width: 500px;>' + arr[i].house + '  <img src=' + arr[i].image + ' alt=' + arr[i].house + '>házak tagja ' + arr[i].sum + ' </div>';
 // }
