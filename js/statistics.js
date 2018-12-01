aaa0f5141041efa76049a2f856e91605a093dca4
lammoni <lammoni85@gmail.com>
2018-12-01 13:10:05 +0100

    házak címerekkel

--- js/statistics.js
+++ js/statistics.js
@@ -7,17 +7,32 @@ function housesList() {
       }
     }
   }
-
-
+  t.sort();
   return t;
+}
+
+function housesInit() {
+  var obj = {};
+  var arr = [];
+  var houses = housesList();
+  for (var i = 0; i < houses.length; i++) {
+    obj = {};
+    obj.house = houses[i];
+    obj.image = 'img/houses/' + houses[i] + '.png';
+    obj.sum = 0;
+    arr.push(obj);
+  }
+  return arr;
 }
 
 // végrehajtás
 housesList();
+housesInit();
 
 // write
-t = housesList();
-for (i = 0; i < t.length; i++) {
-  document.querySelector('#solution').innerHTML +=  t[i]+'<br> ';
+arr = housesInit();
+
+for (i = 0; i < arr.length; i++) {
+  document.querySelector('#solution').innerHTML += '<div width: 500px;>' + arr[i].house + '  <img src=' + arr[i].image + ' alt=' + arr[i].house + '>házak tagja' + arr[i].sum + ' </div>';
 }
 
