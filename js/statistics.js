047e9b60b5c95c4afefd76f85a92d91e62edf92c
lammoni <lammoni85@gmail.com>
2018-12-01 14:59:51 +0100

    élők és holtak listázása

--- js/statistics.js
+++ js/statistics.js
@@ -51,17 +51,45 @@ function writeHouses() {
   document.querySelector('#solution').innerHTML = szoveg;
 }
 
+
+function deadList() {
+  var t = [];
+  for (var i = 0; i < gameOfThronesCharacters.length; i++) {
+    if (gameOfThronesCharacters[i].dead) {
+      if (!t.includes(gameOfThronesCharacters[i].dead)) {
+        t.push(gameOfThronesCharacters[i].name);
+      }
+    }
+  }
+  t.sort();
+  return t;
+}
+
+function aliveList() {
+  var al = [];
+  var szoveg = '';
+  for (var i = 0; i < gameOfThronesCharacters.length; i++) {
+    if ( !gameOfThronesCharacters[i].dead) {
+      al.push(gameOfThronesCharacters[i].name);
+    }
+  }
+  al.sort();
+  return al;
+}
 // végrehajtás
 // housesList();
 // housesInit();
-//housesSum();
-document.querySelector('#solution').innerHTML = '';
-btnElement=document.getElementById('showHouses');
-btnElement.addEventListener('click', writeHouses() );
-//writeHouses();
+// housesSum();
+deadList();
+// document.querySelector('#solution').innerHTML = '';
+// btnElement = document.getElementById('showHouses');
+// btnElement.addEventListener('click', writeHouses() );
+// writeHouses();
 // write
-//arr = housesSum();
-
+arr = aliveList();
+for (i = 0; i < arr.length; i++) {
+    document.querySelector('#solutions').innerHTML +=arr[i]+' <br>';
+}
 //for (i = 0; i < arr.length; i++) {
  // document.querySelector('#solution').innerHTML += '<div width: 500px;>' + arr[i].house + '  <img src=' + arr[i].image + ' alt=' + arr[i].house + '>házak tagja ' + arr[i].sum + ' </div>';
 //}
